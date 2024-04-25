e0e412f8c163a17e33a6093b2877913616f5aee5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-25 12:01:14 +0200

    Zastosowanie parametryzacji testów i testu ogólnego

--- src/TestApp.UnitTests/MarkdownFormatterTests.cs
+++ src/TestApp.UnitTests/MarkdownFormatterTests.cs
@@ -1,29 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 using Assert = Xunit.Assert;
 
 namespace TestApp.UnitTests
 {
+    // dotnet add package xunit
+    // dotnet add package xunit.runner.visualstudio 
+
     public class MarkdownFormatterTests
     {
         // Method_Scenario_ExpectedBehavior
 
-        [Fact]
-        public void FormatAsBold_NotEmptyContent_ReturnsContentEncloseDoubleAsterix()
+        [Theory]
+        [InlineData("a")]
+        [InlineData("abc")]
+        [InlineData("Lorem ipsum")]
+        public void FormatAsBold_NotEmptyContent_ReturnsContentEncloseDoubleAsterix(string content)
         {
             // Arrange
             MarkdownFormatter formatter = new MarkdownFormatter();
 
             // Act
-            var result = formatter.FormatAsBold("a");
+            var result = formatter.FormatAsBold(content);
 
             // Assert
-            Assert.Equal("**a**", result);
+
+            // Wariant 1: Test szczegółowy
+            // Assert.Equal(expected, result); 
+
+            // Wariant 2: Test ogólny
+            Assert.StartsWith("**", result);
+            Assert.Contains(content, result);
+            Assert.EndsWith("**", result);
         }
+
 
         [Fact]
         public void FormatAsBold_EmptyContent_ShouldThrowArgumentNullException()
