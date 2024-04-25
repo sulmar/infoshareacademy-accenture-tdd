1f6a1d5b8bef84f1b5521f61a627b1a6b6a3a9d3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-25 08:36:02 +0200

    Zmiana przykładu

---
+++ src/TestApp/Fundamentals/TemperatureSensor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TestApp.Fundamentals;
+
+// Testing Events
+public class TemperatureSensor
+{
+    private const float HighTemperatureThreshold = 30.0f;
+
+    public event EventHandler<TemperatureEventArgs> OnHighTemperatureAlert;
+    private float _temperature;
+
+    public void SetTemperature(float temperature)
+    {
+        _temperature = temperature;
+        Console.WriteLine($"Current Temperature: {_temperature}°C");
+
+        if (_temperature > HighTemperatureThreshold)
+        {
+            OnHighTemperatureAlert?.Invoke(this, new TemperatureEventArgs(_temperature));
+        }
+    }
+}
+
+
+public class TemperatureEventArgs : EventArgs
+{
+    public float Temperature { get; }
+
+    public TemperatureEventArgs(float temperature)
+    {
+        Temperature = temperature;
+    }
+}
