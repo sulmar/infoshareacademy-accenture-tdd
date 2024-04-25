b790c3bfecb48935a41f31da1c83f2687904482b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-25 17:07:39 +0200

    Dodanie testów

---
+++ src/TestApp.UnitTests/ColorHelperTests.cs
@@ -0,0 +1,34 @@
+﻿using TestApp.Fundamentals;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace TestApp.UnitTests;
+
+public class ColorHelperTests
+{
+    [Fact]
+    public void DetermineColor_BelowPercentageZero_ShouldReturnsThrowsArgumentRangeException()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    [Fact]
+    public void DetermineColor_AbovePercentage100_ShouldReturnsThrowsArgumentRangeException()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Theory]
+    [InlineData(0, "Green")]
+    [InlineData(49, "Green")]
+    [InlineData(50, "Yellow")]
+    [InlineData(80, "Yellow")]
+    [InlineData(81, "Red")]
+    public void DetermineColor_Threshold_ShouldReturnsExpectedColor(int value, string expected)
+    {
+        var result = ColorConverter.ConvertColor(value);
+
+        Assert.Equal(expected, result); 
+    }
+}
--- src/TestApp/Fundamentals/ColorHelper.cs
+++ src/TestApp/Fundamentals/ColorHelper.cs
@@ -1,16 +1,16 @@
 ﻿namespace TestApp.Fundamentals;
 
 // Boundary Test
-public class ColorHelper
+public class ColorConverter // Adapter Design Pattern
 {
-    static string DetermineColor(int percentage)
+    public static string ConvertColor(int percentage)
     {
         // Thresholds
         if (percentage >= 0 && percentage < 50)
         {
             return "Green";
         }
-        else if (percentage > 50 && percentage <= 80)
+        else if (percentage >= 50 && percentage <= 80)
         {
             return "Yellow";
         }
