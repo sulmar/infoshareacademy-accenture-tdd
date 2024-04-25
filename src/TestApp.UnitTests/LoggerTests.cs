c876c1d6916a55a0e524f48d4dd8b59a21db2964
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-25 12:51:44 +0200

    Utworzenie przypadków testowych

---
+++ src/TestApp.UnitTests/LoggerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestApp.UnitTests;
+
+public class LoggerTests
+{
+    [Fact]
+    public void Log_MessageIsEmpty_ShouldThrowsArgumentNullException()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void Log_MessageIsNotEmpty_ShouldSetLastMessage()
+    {
+        throw new NotImplementedException();
+    }
+}
