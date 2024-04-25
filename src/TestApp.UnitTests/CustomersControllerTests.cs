4ad115affcdeb7400b7b5cc3d43cebdb60eebe21
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-25 12:39:33 +0200

    Utworzenie testów

---
+++ src/TestApp.UnitTests/CustomersControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Fundamentals;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace TestApp.UnitTests;
+
+public class CustomersControllerTests
+{
+
+    [Fact]
+    public void GetCustomer_CustomerIdIsZero_ShouldReturnsNotFound()
+    {
+        // Arrange
+        CustomersController customersController = new CustomersController();
+
+        // Act
+        var result = customersController.GetCustomer(0);
+
+        // Assert
+        Assert.IsType<NotFound>(result);
+        
+    }
+
+    [Fact]
+    public void GetCustomer_CustomerIdIsNotZero_ShouldReturnsOk()
+    {
+        // Arrange
+        CustomersController customersController = new CustomersController();
+
+        // Act
+        var result = customersController.GetCustomer(1);
+
+        // Assert
+        Assert.IsType<Ok>(result);
+    }
+}
