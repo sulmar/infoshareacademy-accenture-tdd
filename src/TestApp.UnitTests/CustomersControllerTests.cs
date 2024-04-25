2a3e5a3ef312e43399816fe03e9bfb0b9361027a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-25 12:42:30 +0200

    refactor: Refaktoring testu

--- src/TestApp.UnitTests/CustomersControllerTests.cs
+++ src/TestApp.UnitTests/CustomersControllerTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TestApp.Fundamentals;
+﻿using TestApp.Fundamentals;
 using Xunit;
 using Assert = Xunit.Assert;
 
@@ -11,29 +6,32 @@ namespace TestApp.UnitTests;
 
 public class CustomersControllerTests
 {
+    CustomersController sut;
+
+    public CustomersControllerTests()
+    {
+        sut = new CustomersController();
+    }
 
     [Fact]
     public void GetCustomer_CustomerIdIsZero_ShouldReturnsNotFound()
     {
         // Arrange
-        CustomersController customersController = new CustomersController();
 
         // Act
-        var result = customersController.GetCustomer(0);
+        var result = sut.GetCustomer(0);
 
         // Assert
-        Assert.IsType<NotFound>(result);
-        
+        Assert.IsType<NotFound>(result);        
     }
 
     [Fact]
     public void GetCustomer_CustomerIdIsNotZero_ShouldReturnsOk()
     {
         // Arrange
-        CustomersController customersController = new CustomersController();
 
         // Act
-        var result = customersController.GetCustomer(1);
+        var result = sut.GetCustomer(1);
 
         // Assert
         Assert.IsType<Ok>(result);
