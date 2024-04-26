4a1eb37f5870fd35dfd7826d59cbf0395a120fff
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-26 17:00:51 +0200

    Dodanie testu metody asynchronicznej

---
+++ src/TestApp.UnitTests/PostServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Fundamentals;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace TestApp.UnitTests;
+
+public class PostServiceTests
+{
+    [Fact]
+    public async Task FetchData_WhenCalled_ShouldReturnsResponse()
+    {
+        // Arrange
+        PostService sut = new PostService();
+
+        // Act
+        var result = await sut.FetchData("http://a.com");
+
+        // Assets
+        Assert.NotNull(result);
+    }
+}
