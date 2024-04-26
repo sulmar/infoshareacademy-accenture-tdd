7d7f2d677cd4b084893d2bbba1ed7b76f25d3968
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-26 13:00:05 +0200

    Dodanie drukarki ZPL

---
+++ src/TestApp.UnitTests/ZplPrinterTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Fundamentals;
+using Xunit;
+using Assert = Xunit.Assert;
+using FluentAssertions;
+
+namespace TestApp.UnitTests;
+
+public class ZplPrinterTests
+{
+
+    [Fact]
+    public void CreateLabel_ValidSize_ShouldCreateLabel()
+    {
+        var sut = new ZplPrinter();
+
+        sut.CreateLabel(20, 10);
+
+        sut.Output.Should()
+         .StartWith("^XA")
+         .And.EndWith("^XZ");
+    }
+
+    [Fact]
+    public void SetText_NotEmptyText_ShouldReturnCommandText()
+    {
+        var sut = new ZplPrinter();
+        sut.CreateLabel(20, 10);
+
+        sut.SetText("Hello World");
+
+        sut.Output.Should()
+            .StartWith("^XA")
+            .And.Contain("^FDHello World^FS")
+            .And.EndWith("^XZ");
+
+    }
+
+    [Fact]
+    public void SetText_EmptyText_ShouldThrowsArgumentNullException()
+    {
+        var sut = new ZplPrinter();
+
+        Action act = () => sut.SetText(string.Empty);
+
+        act.Should().Throw<ArgumentNullException>();    
+    }
+
+    [Fact]
+    public void SetPosition_ValidPosition_ShouldReturnCommandText()
+    {
+        var sut = new ZplPrinter();
+        sut.CreateLabel(20, 10);
+
+        sut.SetPosition(1, 2);
+
+        sut.Output.Should()
+            .StartWith("^XA")
+            .And.Contain("^FO1,2")
+            .And.EndWith("^XZ");
+
+    }
+}
---
+++ src/TestApp/Fundamentals/ZplPrinter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Fundamentals;
+
+public class ZplPrinter
+{
+    private readonly StringBuilder commandBuilder = new StringBuilder();
+
+    public void CreateLabel(int width, int height)
+    {
+        commandBuilder.Append("^XA");
+    }
+
+    public void SetText(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            throw new ArgumentNullException("text");
+
+        commandBuilder.Append("^FD");
+        commandBuilder.Append(text);
+        commandBuilder.Append("^FS");
+    }
+
+
+    public void SetPosition(int x, int y)
+    {
+        commandBuilder.Append($"^FO{x},{y}");
+    }
+
+    public string Output
+    {
+        get
+        {
+            commandBuilder.Append("^XZ");
+
+            return commandBuilder.ToString();
+        }
+    }
+
+
+}
