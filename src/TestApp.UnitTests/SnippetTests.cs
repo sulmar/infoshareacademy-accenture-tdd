5d616f15b4b68aa699254c5c120618609060045f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-26 16:59:46 +0200

    Przykład użycia snippetów

---
+++ src/TestApp.UnitTests/SnippetTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace TestApp.UnitTests;
+
+public class SnippetTests
+{
+
+	[Fact]
+	public void DoWork_WhenCalled_ShouldReturnsZero()
+	{
+		// Arrange
+
+		// Act
+		throw new NotImplementedException();
+
+		// Assets
+
+	}
+
+
+}
